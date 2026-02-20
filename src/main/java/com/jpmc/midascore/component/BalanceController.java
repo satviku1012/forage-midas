ac6dc149e9492a4deedfa7beb89914bd6c6b5e15
Satvik Utkoori <satvikutkoori@Satviks-MacBook-Pro-2.local>
2026-02-20 14:22:03 -0600

    Added a /get endpoint to provide user balances

---
+++ src/main/java/com/jpmc/midascore/component/BalanceController.java
@@ -0,0 +1,31 @@
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Balance;
+import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BalanceController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public BalanceController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/balance")
+    public Balance getBalance(@RequestParam Long userId) {
+        UserRecord user = userRepository.findById(userId).orElse(null);
+
+        if (user == null) {
+            return new Balance(0);
+        } else {
+            return new Balance(user.getBalance());
+        }
+    }
+}
